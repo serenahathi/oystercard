0b37527917faa1efd6db15ad509ce66df544239a
Serena Hathi <hathi.serena@gmail.com>
2018-01-29 17:03:50 +0000

    Challenge 5: add ability to top up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,6 +6,14 @@ describe Oystercard do
     it 'when initialized has a balance of 0' do
       expect(subject.balance).to eq 0
     end
-
   end
+
+  context 'top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'adjusts balance by top up amount' do
+      expect { subject.top_up(1) }.to change { subject.balance }.by(1)
+    end
+    
+  end  
 end
