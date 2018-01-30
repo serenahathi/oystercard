89afc1cbb18e6816033fd364d2e8e542a112e82d
Serena Hathi <hathi.serena@gmail.com>
2018-01-30 15:54:27 +0000

    Chapter 8 - Touch in and out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,13 +3,26 @@ class Oystercard
   BALANCE_LIMIT = 90
 
   attr_reader :balance
-
-  def initialize(balance = 0)
+  
+  def initialize(balance = 0, in_journey = nil)
     @balance = balance
+    @in_journey = in_journey
+  end
+
+  def in_journey?
+    @in_journey == true
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
   def top_up(amount)
-    fail "Error - maximum balance of #{BALANCE_LIMIT} pounds" if (@balance + amount > BALANCE_LIMIT)
+    fail "Error - maximum balance is #{BALANCE_LIMIT} pounds" if (@balance + amount > BALANCE_LIMIT)
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,37 +2,59 @@ require 'oystercard'
 
 describe Oystercard do
 
-  context 'balance' do
+  subject(:card) { described_class.new}
+
+  describe 'card' do
+    it 'has a default status of' do
+    expect(card).to have_attributes(:balance => 0)
+    end
+  end
+
+  describe '#touch in' do
+    it { is_expected.to respond_to(:touch_in) }
+
+    it 'changes status to true' do
+      card.touch_in
+      expect(card).to be_in_journey
+    end
+  end
+
+  describe '#touch out' do
+    it { is_expected.to respond_to(:touch_out) }
+
+    it 'changes status to false' do
+      card.touch_in  
+      card.touch_out
+      expect(card).not_to be_in_journey
+    end
+  end
+
+  describe 'balance' do
     it 'when initialized has a balance of 0' do
-      expect(subject.balance).to eq 0
+      expect(card.balance).to eq 0
     end
-  end
-
-  context 'top_up' do
-
-    it 'adjusts balance by top up amount' do
-      expect { subject.top_up(1) }.to change { subject.balance }.by(1)
-    end
-  end
-    
-  context 'maximum balance' do
-    # it 'raises error when Oystercard balance is greater than 90 pounds' do
-    #   subject.top_up(80)
-    #   expect { subject.top_up(21) }.to raise_error("Error - maximum balance is 90 pounds")
-    # end
 
     it 'raises error when Oystercard balance is greater than 90 pounds' do
       balance_limit = Oystercard::BALANCE_LIMIT
-      subject.top_up(balance_limit)
-      expect { subject.top_up(1) }.to raise_error("Error - maximum balance of #{balance_limit} pounds")
-    end
+      card.top_up(balance_limit)
+      message = "Error - maximum balance is #{balance_limit} pounds"
+      expect { card.top_up(1) }.to raise_error(message)
+    end 
   end
-  
-  context 'deducts amount' do
 
-    it 'deducts fare' do
-      subject.top_up(10)
-      expect { subject.deduct(5) }.to change { subject.balance }.by(-5)
+  describe '#top_up' do
+    it 'adjusts balance by top up amount' do
+      expect { card.top_up(1) }.to change { card.balance }.by(1)
     end
   end
+     
+  describe '#deduct' do
+    it 'deducts fare' do
+      card.top_up(10)
+      expect { card.deduct(5) }.to change { card.balance }.by(-5)
+    end
+
+
+  end
+
 end
